6b337aa10439c2ffd6b29b96ff4356385885f137
Jean M Cetoute <jeanmarylerate51@gmail.com>
2019-09-12 21:05:44 -0500

    setting up the route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
+import {Switch, Route} from "react-router-dom";
+import whiskey from "./imgs/whiskey.jpg";
+import tubby from "./imgs/tubby.jpg";
+import hazel from "./imgs/hazel.jpg";
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-       <h1 className="display-1">Hello</h1>
-    </div>
-  );
+class App extends Component {
+  static defaultProps = {
+    dogs: [
+      {
+        name: "Whiskey",
+        age: 5,
+        src: whiskey,
+        facts: [
+          "Whiskey loves eating popcorn.",
+          "Whiskey is a terrible guard dog.",
+          "Whiskey wants to cuddle with you!"
+        ]
+      },
+      {
+        name: "Hazel",
+        age: 3,
+        src: hazel,
+        facts: [
+          "Hazel has soooo much energy!",
+          "Hazel is highly intelligent.",
+          "Hazel loves people more than dogs."
+        ]
+      },
+      {
+        name: "Tubby",
+        age: 4,
+        src: tubby,
+        facts: [
+          "Tubby is not the brightest dog",
+          "Tubby does not like walks or exercise.",
+          "Tubby loves eating food."
+        ]
+      }
+    ]
+  }
+  render() {
+    return (
+     <Route exact path="/dogs" render={() => <h1>DOG LIST</h1>}></Route>
+    )
+  }
 }
-
 export default App;
