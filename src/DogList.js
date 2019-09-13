1cfe3e37b75d63f0d9d0ea1622609e1d34fba043
Jean M Cetoute <jeanmarylerate51@gmail.com>
2019-09-13 16:42:04 -0500

    finish product

--- src/DogList.js
+++ src/DogList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
 import "./DogList.css";
 
 
@@ -6,20 +7,18 @@ export default class DogList extends Component {
     render() {
         return (
             <div className="DogList">
-                <h1 className="display-1 text-center">Dog List!</h1>
-                <div className="container">
+                <h1 className="display-1 text-center my-4">Dog List!</h1>
                     <div className="row">
                          {this.props.dogs.map(d => (
                              <div className="Dog col-lg-4 text-center" key={d.name}>
                                  <img src={d.src} alt={d.name}/>
-                                 <h3>{d.name}</h3>
+                                 <h3><Link className="underline"to={`/dogs/${d.name}`}>{d.name}</Link> </h3>
                              </div>
                          ))}
 
                     </div>
 
                 </div>
-            </div>
         )
     }
 }
